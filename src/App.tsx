2319da434cc763d1a96ba701f719ff4c7c7f88b1
MorganPendragon <vincentagustin123@gmail.com>
2025-09-17 01:26:15 +0800

    init bug

--- src/App.tsx
+++ src/App.tsx
@@ -9,17 +9,14 @@ function App() {
         alt="Example"
         className="w-[100%]"
       />
-      <iframe
-        title="F1 2025 Ferrari SF-25"
-        src="https://sketchfab.com/models/7929bd7771d8494eaf0eb31404e24bf6/embed?autostart=1&preload=1&transparent=1&ui_theme=dark"
-        style={{
-          width: '1900px',
-          height: '1080px',
-          border: 'none',
-        }}
-        allow="autoplay; fullscreen; xr-spatial-tracking"
-        allowFullScreen
-      ></iframe>
+      <div className="model">
+        <iframe
+          title="F1 2025 Ferrari SF-25"
+          src="https://sketchfab.com/models/7929bd7771d8494eaf0eb31404e24bf6/embed?autostart=1&preload=1&transparent=1&ui_theme=dark"
+          allow="autoplay; fullscreen; xr-spatial-tracking"
+          allowFullScreen
+        ></iframe>
+      </div>
     </div>
   );
 }
